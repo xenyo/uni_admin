0e30172688aed09c67bfb13c54b1554343311ef6
Parkle Lee <parkle.lee@xenyo.com>
2022-11-29 18:30:41 +0800

    Clean up form alter hook

--- functions/form_alter.php
+++ functions/form_alter.php
@@ -10,23 +10,6 @@ function uni_admin_form_alter(&$form, $form_state, $form_id) {
    * Layout paragraphs component form
    */
   if ($form_id === 'layout_paragraphs_component_form') {
-    // Attach libraries
     $form['#attached']['library'][] = 'uni_admin/layout-paragraphs-component-form';
-
-    // Modify select empty option labels
-    foreach (Element::children($form) as $key) {
-      if (isset($form[$key]['widget'][0]['select'])) {
-        $form[$key]['widget'][0]['select']['#empty_option'] = t('- Auto -');
-      }
-    }
-
-    // Add form states
-    $form["uni_wrapper_width"]['#states'] = [
-      'invisible' => [
-        ":input[name='uni_wrapper_disable[value]']" => [
-          'checked' => true,
-        ],
-      ],
-    ];
   }
 }
