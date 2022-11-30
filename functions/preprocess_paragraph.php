1821898e563ae7cac218d7b0823f612c01737604
Parkle Lee <parkle.lee@xenyo.com>
2022-11-30 10:53:35 +0800

    Add suggestion classes on layout and paragraph entities

---
+++ functions/preprocess_paragraph.php
@@ -0,0 +1,10 @@
+<?php
+
+use Drupal\Core\Template\AttributeHelper;
+
+function uni_admin_preprocess_paragraph(&$variables) {
+  $variables['attributes'] = AttributeHelper::mergeCollections(
+    $variables['attributes'] ?? [],
+    [ 'class' => $variables['suggestions'] ],
+  );
+}
